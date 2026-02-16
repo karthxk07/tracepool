47e93a28b59bb6695d113835535bb0a627366d4a
karthxk07 <karthikkalyanam07@gmail.com>
2026-02-16 16:43:16 +0530

    changed to node

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config();
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+
+app.get("/", async (req, res) => {
+  console.log(req.ip);
+
+  const { error } = await supabase.from("logger").insert([{ ip: String(req.ip) }]);
+
+  if (error) {
+    console.log(error);
+  }
+
+  res.end()
+})
+
+
+
+app.listen(3000, () => { "server started on port 3000" });
